83ca3c62f4d5928cd8d0fef1be79ad2b960f96c2
findit <53036391+finditDeveloper@users.noreply.github.com>
2019-12-17 11:23:45 +0200

    inital commit

--- src/screens/Events.js
+++ src/screens/Events.js
@@ -1,20 +1,52 @@
 import React from "react";
-import { View, Text, FlatList } from "react-native";
+import { View, Text, FlatList, SafeAreaView, StyleSheet } from "react-native";
 import { connect } from "react-redux";
+import { CardEcomFour } from "react-native-card-ui";
 
 const DATA = [
-    {
-        
-    }
-]
+  {
+    img: "",
+    title: "",
+    icon: "",
+    kay: "hdf236eu"
+  }
+];
 
 const Events = () => {
+  RenderItem = ({ data }) => {
+    return (
+      <CardEcomFour
+        title={"Porsche Rubber"}
+        subTitle={
+          "Zermatt is famed as a mounering and ski destome banmdo liono"
+        }
+        price={""}
+        image={{uri:'https://res.cloudinary.com/chawanangwa/image/upload/v1576282284/66342585_2387694371468181_6021474935594024960_n_nrjbqe.jpg'}}
+        buttonText={"VIEW DETAILS"}
+        buttonColor={"#4383FF"}
+        onClickButton={() => alert("Has clicked")}
+      />
+    );
+  };
+
   return (
-    <View>
-      
-    </View>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => <RenderItem data={item} />}
+      />
+    </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 
 Events.navigationOptions = {
   header: null
