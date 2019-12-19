e310ee5de1b114fbe30f0fd5f12aff55662f75ad
findit <53036391+finditDeveloper@users.noreply.github.com>
2019-12-20 01:11:44 +0200

    inital commit

--- src/components/ToolBar.js
+++ src/components/ToolBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { View, Text, StatusBar, Dimensions, Platform } from "react-native";
 import { Toolbar } from "react-native-material-ui";
+import { Ionicons } from "@expo/vector-icons";
 
 const X_WIDTH = 375;
 const X_HEIGHT = 812;
@@ -22,12 +23,38 @@ export const StatusBarHeight = Platform.select({
   default: 0
 });
 
-const ToolBar = () => {
+const ToolBar = props => {
   return (
     <View>
       <StatusBar barStyle="light-content" />
-      <View style={{ height: StatusBarHeight, backgroundColor: "#503088" }} />
-      <View
+      <View style={{ height: StatusBarHeight, backgroundColor: "#4C0D6B" }} />
+      <Toolbar
+        leftElement="menu"
+        centerElement={ 
+          <Text
+            style={{
+              fontFamily: "Roboto-Medium",
+              color: "#E3E2E3",
+              fontSize: 20
+            }}
+          >
+            Home
+          </Text>
+        }
+        rightElement={{
+          menu: {
+            icon: "more-vert",
+            labels: ["item 1", "item 2"]
+          }
+        }}
+        onRightElementPress={label => {
+          console.log(label);
+        }}
+        style={{
+          container: { backgroundColor: "#6B4180" }
+        }}
+      />
+      {/* <View
         style={{
           height: 25,
           padding: 2,
@@ -36,7 +63,7 @@ const ToolBar = () => {
         }}
       >
         <Text style={{ color: "#DFDFDF" }}>Dravens HelthCare</Text>
-      </View>
+      </View> */}
     </View>
   );
 };
