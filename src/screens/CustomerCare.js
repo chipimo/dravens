d07cffdd3637d00649fe359853f9b8e2c9f600a2
Melvin Chipimo <chipimo31@gmail.com>
2019-12-20 10:10:14 +0200

    Update CustomerCare.js

--- src/screens/CustomerCare.js
+++ src/screens/CustomerCare.js
@@ -1,12 +1,43 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { GiftedChat } from 'react-native-gifted-chat'
 
-const CustomerCare = () => {
+class CustomerCare extends React.Component {
+  state = {
+    messages: [],
+  }
+
+  componentDidMount() {
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: 'Hello developer',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ],
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  render() {
     return (
-        <View>
-            <Text></Text>
-        </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={messages => this.onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
     )
-}
-
-export default CustomerCare
+  }
+} 
