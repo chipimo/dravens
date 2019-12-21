7166a6ed04c9bcdac3743b7ebaf43198173fdd56
Melvin Chipimo <chipimo31@gmail.com>
2019-12-21 12:04:24 +0000

    home cards change

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -30,6 +30,24 @@ const DATA = [
       "https://res.cloudinary.com/chawanangwa/image/upload/v1576282269/66076406_667789507057425_3243528614397870080_n_ztppd4.jpg"
   },
   {
+    type: "sevices",
+    id: "58694a0f-3da1-471f-bd96-1571e29d72",
+    title: "HealthCare Services",
+    data: [
+      {
+        id: "58694a0f-3da1-471f-b96-145571e242",
+        title: "MEAL PREPARATION",
+        summary:
+          "We are a specialist care service providing " +
+          "the home care services and support to " +
+          "individuals living in the comfort of their " +
+          "own homes",
+        contant: "",
+        icon: require("../assets/imageIcons/icons8-vegan-food-64.png")
+      }
+    ]
+  },
+  {
     id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
     title: "Second Item",
     type: "Events",
@@ -48,64 +66,6 @@ const DATA = [
         title: "Safeguarding vulnerable adults",
         img:
           "https://res.cloudinary.com/chawanangwa/image/upload/v1576282284/66342585_2387694371468181_6021474935594024960_n_nrjbqe.jpg"
-      },
-      {
-        id: "58694a0f-3da1-471f-bd96-14557561e29d0",
-        summary: "",
-        subTitle: "What is Safeguarding Training & Why is it so Important?",
-        author: "Dravens",
-        title: "Care Certificate",
-        img:
-          "https://res.cloudinary.com/chawanangwa/image/upload/v1576567350/66258261_432012277640677_254627926417342464_n_au2gp5.jpg"
-      },
-      {
-        id: "58694a0f-3da1-471f-bd96-145571e35fv9d0",
-        subTitle: "What is Safeguarding Training & Why is it so Important?",
-        summary: "",
-        author: "Dravens",
-        title: "Challenging behavior",
-        img:
-          "https://res.cloudinary.com/chawanangwa/image/upload/v1576567474/66127061_957336111332158_1926395640888164352_n_qsxwkx.jpg"
-      }
-    ]
-  },
-  {
-    type: "sevices",
-    id: "58694a0f-3da1-471f-bd96-1571e29d72",
-    title: "HealthCare Services",
-    data: [
-      {
-        id: "58694a0f-3da1-471f-b96-145571e242",
-        title: "MEAL PREPARATION",
-        summary:
-          "We are a specialist care service providing " +
-          "the home care services and support to " +
-          "individuals living in the comfort of their " +
-          "own homes",
-        contant: "",
-        icon: require("../assets/imageIcons/icons8-vegan-food-64.png")
-      },
-      {
-        id: "58694a0f-3da1-471f-bd96-145571e22",
-        title: "MEDICATION SUPPORT",
-        summary:
-          "We are a specialist care service providing " +
-          "the home care services and support to " +
-          "individuals living in the comfort of their " +
-          "own homes",
-        contant: "",
-        icon: require("../assets/imageIcons/icons8-pills-64.png")
-      },
-      {
-        id: "58694a0f-da1-471f-bd96-14551e242",
-        title: "HOMECARE",
-        summary:
-          "We are a specialist care service providing " +
-          "the home care services and support to " +
-          "individuals living in the comfort of their " +
-          "own homes",
-        contant: "",
-        icon: require("../assets/imageIcons/icons8-hospital-bed-80.png")
       }
     ]
   },
@@ -133,12 +93,33 @@ const DATA = [
 ];
 
 const HomeScreen = props => {
+  const initalBlogState = [
+    {
+      id: "5869f-da1-471f-bd96-14551e242",
+      title: "Focus on prevention.",
+      summary:
+        "We are a specialist care service providing " +
+        "the home care services and support to " +
+        "individuals living in the comfort of their " +
+        "own homes",
+      contant: "",
+      image:
+        "https://res.cloudinary.com/chawanangwa/image/upload/v1576282257/62515058_3145476662137095_4364052734014390272_n_uzxn15.jpg",
+      icon:
+        "https://lemag.nikonclub.fr/wp-content/uploads/2016/11/Photo-selection-pour-Nikon-France-Mattia-Bonavida-2016-6.jpg"
+    }
+  ];
   const [hour, sethour] = React.useState(null);
   const [username, setusername] = React.useState("Melvin");
+  const [blogPost, setBlogPost] = React.useState(initalBlogState);
   const value = useSelector(store => store.EventCard.isLoaded);
 
   useEffect(() => {
+    // set time
     this.getHour();
+    // get blog post
+    this.getBlogPost();
+
     if (value) props.navigation.navigate("EventViewer");
 
     return () => {};
@@ -150,49 +131,38 @@ const HomeScreen = props => {
     sethour(hour);
   };
 
+  getBlogPost = () => {
+    fetch("https://dravens.co.uk/wp-json/wp/v2/posts/")
+      .then(response => {
+        response.json();
+      })
+      .then(data => {
+        let dataPost = [];
+        data.results.map(item => {
+          dataPost.push(item);
+        });
+        setBlogPost(dataPost);
+      });
+  };
+
   RenderItemInner = ({ data }) => {
     return (
-      <TouchableOpacity
-        onPress={() => {
-          props.dispatchEvent({
-            type: "EVENTITEM",
-            payload: {
-              img: data.img,
-              title: data.title,
-              author: data.author,
-              subTitle: data.subTitle,
-              summary: data.summary
-            }
-          });
+      <CardEcomFour
+        title={data.title}
+        subTitle={data.subTitle}
+        price={""}
+        image={{
+          uri: data.img
         }}
-        style={{
-          width: scale(150),
-          marginLeft: 10,
-          marginRight: 10,
-          height: "100%",
-          overflow: "hidden"
-        }}
-      >
-        <View style={{ height: verticalScale(90), width: "100%" }}>
-          <ImageBackground
-            source={{
-              uri: data.img
-            }}
-            style={{
-              width: "100%",
-              height: "100%",
-              borderRadius: 5,
-              overflow: "hidden"
-            }}
-          ></ImageBackground>
-        </View>
-        <Text
-          numberOfLines={2}
-          style={{ fontFamily: "Roboto-Light", color: "#515151" }}
-        >
-          {data.title}
-        </Text>
-      </TouchableOpacity>
+        buttonText={"VIEW DETAILS"}
+        buttonColor={"#4383FF"}
+        onClickButton={() =>
+          props.navigation.navigate("LinkScreen", {
+            title: "Traning",
+            link: "https://findithomes.com/reservations/"
+          })
+        }
+      />
     );
   };
 
@@ -222,8 +192,8 @@ const HomeScreen = props => {
   RenderFeeds = ({ data }) => {
     return (
       <CardSix
-        title={data.title}
-        subTitle={data.summary}
+        title={data.title.rendered}
+        subTitle={data.content.rendered}
         stars={234}
         profile={{
           uri: data.icon
@@ -269,94 +239,99 @@ const HomeScreen = props => {
           </View>
         ) : (
           <View>
-            {data.type === "Events" ? (
-              <View style={{ height: 210, padding: 5 }}>
-                <View style={{ marginBottom: 5, marginLeft: 10 }}>
-                  <View>
-                    <TouchableOpacity
-                      onPress={() => {
-                        props.dispatchEvent({
-                          type: "NAVTO",
-                          routeName: "EventViewer",
-                          payload: { naviget: props.navigation, isModal: false }
-                        });
-                        props.navigation.navigate("Events");
-                      }}
+            {data.type === "sevices" ? (
+              //  HealthCare Services
+              <View>
+                <TouchableOpacity
+                  onPress={() => props.navigation.navigate("ServicesView")}
+                  style={{
+                    marginLeft: 15,
+                    marginTop: 7,
+                    marginBottom: 10
+                  }}
+                >
+                  <View style={{ flexDirection: "row" }}>
+                    <Text
+                      h4
                       style={{
-                        marginLeft: 2,
-                        marginTop: 7,
-                        marginBottom: 4
+                        color: "#AAAAAA",
+                        fontFamily: "Roboto-Bold"
                       }}
                     >
-                      <View style={{ flexDirection: "row" }}>
-                        <Text
-                          h4
-                          style={{
-                            color: "#AAAAAA",
-                            fontFamily: "Roboto-Bold"
-                          }}
-                        >
-                          Training{" "}
-                        </Text>
-                        <View style={{ marginLeft: 5, marginTop: 5 }}>
-                          <Ionicons
-                            name="ios-arrow-dropright"
-                            size={23}
-                            color="#AAAAAA"
-                          />
-                        </View>
-                      </View>
-                      <Text style={{ color: "#AAAAAA" }}>
-                        Our Training is specially delivered In House{" "}
-                      </Text>
-                    </TouchableOpacity>
+                      HealthCare Services{" "}
+                    </Text>
+                    <View style={{ marginLeft: 5, marginTop: 5 }}>
+                      <Ionicons
+                        name="ios-arrow-dropright"
+                        size={23}
+                        color="#AAAAAA"
+                      />
+                    </View>
                   </View>
-                </View>
+                  <Text style={{ color: "#AAAAAA" }}>
+                    Our Training is specially delivered In House
+                  </Text>
+                </TouchableOpacity>
                 <FlatList
-                  horizontal={true}
                   data={data.data}
                   keyExtractor={item => item.id}
-                  renderItem={({ item }) => <RenderItemInner data={item} />}
+                  renderItem={({ item }) => <RenderServices data={item} />}
                 />
               </View>
             ) : (
               <View style={{ marginTop: 10 }}>
-                {data.type === "sevices" ? (
-                  <View>
-                    <TouchableOpacity
-                      onPress={() => props.navigation.navigate("ServicesView")}
-                      style={{
-                        marginLeft: 15,
-                        marginTop: 7,
-                        marginBottom: 10
-                      }}
-                    >
-                      <View style={{ flexDirection: "row" }}>
-                        <Text
-                          h4
+                {data.type === "Events" ? (
+                  // Traninig
+                  <View style={{ height: 210, padding: 5 }}>
+                    <View style={{ marginBottom: 5, marginLeft: 10 }}>
+                      <View>
+                        <TouchableOpacity
+                          onPress={() => {
+                            props.dispatchEvent({
+                              type: "NAVTO",
+                              routeName: "EventViewer",
+                              payload: {
+                                naviget: props.navigation,
+                                isModal: false
+                              }
+                            });
+                            props.navigation.navigate("Events");
+                          }}
                           style={{
-                            color: "#AAAAAA",
-                            fontFamily: "Roboto-Bold"
+                            marginLeft: 2,
+                            marginTop: 7,
+                            marginBottom: 4
                           }}
                         >
-                          HealthCare Services{" "}
-                        </Text>
-                        <View style={{ marginLeft: 5, marginTop: 5 }}>
-                          <Ionicons
-                            name="ios-arrow-dropright"
-                            size={23}
-                            color="#AAAAAA"
-                          />
-                        </View>
+                          <View style={{ flexDirection: "row" }}>
+                            <Text
+                              h4
+                              style={{
+                                color: "#AAAAAA",
+                                fontFamily: "Roboto-Bold"
+                              }}
+                            >
+                              Training{" "}
+                            </Text>
+                            <View style={{ marginLeft: 5, marginTop: 5 }}>
+                              <Ionicons
+                                name="ios-arrow-dropright"
+                                size={23}
+                                color="#AAAAAA"
+                              />
+                            </View>
+                          </View>
+                          <Text style={{ color: "#AAAAAA" }}>
+                            Our Training is specially delivered In House{" "}
+                          </Text>
+                        </TouchableOpacity>
                       </View>
-                      <Text style={{ color: "#AAAAAA" }}>
-                        Our Training is specially delivered In House{" "}
-                      </Text>
-                    </TouchableOpacity>
+                    </View>
                     <FlatList
+                      horizontal={true}
                       data={data.data}
                       keyExtractor={item => item.id}
-                      renderItem={({ item }) => <RenderServices data={item} />}
+                      renderItem={({ item }) => <RenderItemInner data={item} />}
                     />
                   </View>
                 ) : (
@@ -367,7 +342,10 @@ const HomeScreen = props => {
                           <View>
                             <TouchableOpacity
                               onPress={() =>
-                                props.navigation.navigate("FeedViewer")
+                                props.navigation.navigate("LinksScreen", {
+                                  title: "Blog",
+                                  link: "https://dravens.co.uk/blog"
+                                })
                               }
                               style={{
                                 marginLeft: 2,
